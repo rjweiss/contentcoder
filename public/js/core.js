4e11407563f6948706fc8907db6258722d4a0f4f
Rebecca Weiss <rebecca.j.weiss@gmail.com>
2014-03-12 01:23:36 -0700

    mucking with styling

--- public/js/core.js
+++ public/js/core.js
@@ -1,4 +1,4 @@
 // js/core.js
 
-angular.module('contentCoder', ['articleController', 'articleService']);
+var contentCoder = angular.module('contentCoder', ['articleController', 'articleService']);
 
