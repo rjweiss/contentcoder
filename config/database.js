bd60f80d6a59d814b115790f4aa0c7eedba2f489
Rebecca Weiss <rebecca.j.weiss@gmail.com>
2014-03-30 12:25:20 -0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 // config/database.js
 
 	module.exports = {
-		url : 'mongodb://ec2-107-20-239-226.compute-1.amazonaws.com:27017/newsfeed'
+		// change url to mongodb location
+		url : 'mongodb://localhost:27017/newsfeed'
 	};
